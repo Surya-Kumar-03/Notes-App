d0214e39ca7294d422035c92ba99bcfd52586bd4
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2022-12-21 16:36:35 +0530

    Created a Container to hold Notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Note></Note>
+      <div className="container">
+        <Note></Note>
+      </div>
       <Footer></Footer>
     </div>
   );
