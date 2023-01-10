f3422a5e037279147c13b381eb2c47fa876f3407
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-10 20:53:00 +0530

    Created Delete Button

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/deletebutton.css"
+
+function DeleteButton() {
+    return (
+        <button className="delete-btn">Delete</button>
+    )
+}
+
+export default DeleteButton;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/note.css";
+import DeleteButton from "./DeleteButton";
 
 function Note(props) {
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <DeleteButton/>
     </div>
   );
 }
